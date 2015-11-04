0c780c84ac8c647778a04747b1166762b602c908
Mark Turner <mark.nick.turner@gmail.com>
2015-11-04 07:55:31 +0000

    Removed body-parser as we're not using it

--- exercises/exercise2/example/app.js
+++ exercises/exercise2/example/app.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended:true}));
 
 var whereIsRoute = require('./routes/whereIsRoutes.js')();
 app.use('/api/whereis', whereIsRoute);
